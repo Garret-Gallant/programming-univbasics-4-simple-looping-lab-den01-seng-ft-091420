49edf2820898a0ed45c8c43591142729c3f9ec1d
Garret Gallant <gallanttechnological@gmail.com>
2020-07-28 00:16:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,15 +24,25 @@ def output_array(array)
   end
 end
 
+# def return_string_array(array)
+#   count = 0
+# while count < array.length do
+#   array[count] = array[count].to_s
+#   count += 1
+# end
+# array
+# end
+
 def return_string_array(array)
   count = 0
-while count < array.length do
-  array[count] = array[count].to_s
-  count += 1
+  new_array = [ ]
+  
+  while count < array.length do
+    array[count].to_s = new_array[count]
+    count += 1
+  end
+  new_array
 end
-array
-end
-
 
 
 
