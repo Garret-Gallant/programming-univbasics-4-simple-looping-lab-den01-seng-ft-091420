edf6fb968d087749dff316a99089bf65b839f887
Garret Gallant <gallanttechnological@gmail.com>
2020-07-28 00:02:41 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,14 @@
 # Write your methods here
 
-def loop_message_5_times(message)
+# def loop_message_5_times(message)
+#   count = 0
+#   while count < 5 do
+#     puts message
+#     count += 1
+#   end
+# end
+
+def loop_message_five_times(message)
   count = 0
   while count < 5 do
     puts message
